c9ca7e22af0ea127447e751a2c061b599d23e636
Dave Lilly <dave@lillys.org>
2022-06-22 18:29:45 -0500

    fix

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,29 +1,32 @@
-import { apiRouteHandler, CommerceAPI, getCommerceAPI as integrationGetCommerceAPI, Category, CommonArgs, CustomerGroup, GetCommerceObjectArgs, GetProductsArgs, Product, Config } from '@amplience/dc-demostore-integration'
+import { apiRouteHandler, CommerceAPI, getCommerceAPI as integrationGetCommerceAPI, Config } from '@amplience/dc-demostore-integration'
 
 // add the /api route
 export default apiRouteHandler
 
 let configuredApi: CommerceAPI
-const initCommerceAPI = async (config: Config) => {
+const initCommerceApi = async (config: Config) => {
     return configuredApi = configuredApi || await integrationGetCommerceAPI(config)
 }
 
-const getCommerceApi = (config: Config): CommerceAPI => ({
-    getProduct: async function (args: GetCommerceObjectArgs): Promise<Product> {
-        return await (await initCommerceAPI(config)).getProduct(args)
-    },
-    getProducts: async function (args: GetProductsArgs): Promise<Product[]> {
-        return await (await initCommerceAPI(config)).getProducts(args)
-    },
-    getCategory: async function (args: GetCommerceObjectArgs): Promise<Category> {
-        return await (await initCommerceAPI(config)).getCategory(args)
-    },
-    getMegaMenu: async function (args: CommonArgs): Promise<Category[]> {
-        return await (await initCommerceAPI(config)).getMegaMenu(args)
-    },
-    getCustomerGroups: async function (args: CommonArgs): Promise<CustomerGroup[]> {
-        return await (await initCommerceAPI(config)).getCustomerGroups(args)
-    }
-})
+// const getCommerceApi = async (config: Config): Promise<CommerceAPI> => {
+//     return await initCommerceAPI(config)
+//     // return {
+//     //     getProduct: async function (args: GetCommerceObjectArgs): Promise<Product> {
+//     //         return await commerceApi.getProduct(args)
+//     //     },
+//     //     getProducts: async function (args: GetProductsArgs): Promise<Product[]> {
+//     //         return await commerceApi.getProducts(args)
+//     //     },
+//     //     getCategory: async function (args: GetCommerceObjectArgs): Promise<Category> {
+//     //         return await commerceApi.getCategory(args)
+//     //     },
+//     //     getMegaMenu: async function (args: CommonArgs): Promise<Category[]> {
+//     //         return await commerceApi.getMegaMenu(args)
+//     //     },
+//     //     getCustomerGroups: async function (args: CommonArgs): Promise<CustomerGroup[]> {
+//     //         return await commerceApi.getCustomerGroups(args)
+//     //     }
+//     // }
+// }
 
-export { initCommerceAPI, getCommerceApi }
+export { initCommerceApi }
