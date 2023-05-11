91e899189d664e66e7aa95b2ac002473c70e8ea4
Neil Mistry <30687441+neilmistryamplience@users.noreply.github.com>
2023-05-11 11:45:29 +0100

    fix: Implemented a fix for matching. If there is no sku data fallback to ID

--- components/ProductSelector/index.tsx
+++ components/ProductSelector/index.tsx
@@ -185,7 +185,15 @@ const ProductSelector: React.FC<AmpSDKProps> = ({ ampSDK }) => {
         } else {
             // TODO: [NOVADEV-980] Update UX to validate if user wants to add variants of a master
             const match = selectedProducts.find(
-                (p) => p.selectedVariant.sku === product.variants[0].sku
+                (p) => {
+                    // if there is a sku
+                    if(  p.selectedVariant.sku ){
+                        return p.selectedVariant.sku === product.variants[0].sku
+                    } else {
+                        // The is no sku so use the ID to match
+                        return p.selectedVariant.id === product.variants[0].id
+                    }
+                }
             )
             if (match) {
                 setAlertMessage("You've already selected this item")
