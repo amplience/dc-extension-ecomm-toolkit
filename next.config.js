5318849ad0c368beb23ba790baab8b7e070e0520
rs-amp <rsimpson@amplience.com>
2023-06-28 16:14:28 +0100

    fix: add unoptimized image flag to allow next export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       return {
